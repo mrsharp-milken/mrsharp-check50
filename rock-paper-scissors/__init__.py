e53fe0953c286469c6ef2865ee713fe4cd502c7c
mrsharp-milken <144956668+mrsharp-milken@users.noreply.github.com>
2025-10-29 17:10:49 -0700

    Update __init__.py

--- rock-paper-scissors/__init__.py
+++ rock-paper-scissors/__init__.py
@@ -7,11 +7,6 @@ def exists():
     check50.exists("rps.py")
     check50.include("testing.py")
 
-@check50.check()
-def exists():
-    """RPS blob exists - Check instructions for new additonal command"""
-    check50.exists(".rps-ansi.txt")
-
 @check50.check(exists)
 def test_invalid_input():
     """rps.py reprompts when user enters invalid input"""
